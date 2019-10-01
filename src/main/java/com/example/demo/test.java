0f6e802ad0a5061d07667030afcbd5764dd879a6
liyue <liyue@carelinker.com>
2019-10-01 13:24:51 +0800

    创建了test文件，第一次提交

---
+++ src/main/java/com/example/demo/test.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @author liyue
+ * @date 2019/10/1 13:23
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("我是第一次提交");
+    }
+}
